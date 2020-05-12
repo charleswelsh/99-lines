75e7dfee6e58c6023a1495b0862d8d16bfdceeae
Charles Welsh <charleswelsh@gmail.com>
2020-05-12 13:23:05 -0500

    added console.log in outer loop for friend notation

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let friendList= ["Dash", "Dan", "Chase", "Ben", "Alex"];
 for (let i = 0; i < friendList.length; i++) {
+    console.log(friendList[i] + ":")
     for (let n = 99; n > 0; n--) {
         if (n === 2) {
             console.log(`${n} lines of code in the file, ${n} lines of code; ${friendList[i]} strikes one out, clears it all out, ${n - 1} line of code in the file`);   
